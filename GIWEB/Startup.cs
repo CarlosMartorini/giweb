805ef86116253d602c2deb61b034cc230541cde0
Carlos Alberto Martorini Silveira <c7.silveira@gmail.com>
2020-08-19 15:10:13 -0300

    Updating views and layout

--- GIWEB/Startup.cs
+++ GIWEB/Startup.cs
@@ -38,6 +38,8 @@ namespace GIWEB
 
             services.AddDbContext<GIWEBContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("GIWEBContext"), builder => builder.MigrationsAssembly("GIWEB")));
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
