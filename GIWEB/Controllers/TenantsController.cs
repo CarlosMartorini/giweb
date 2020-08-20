ba06b0c4adcd6581e053b5b18f35de3a1d1297d9
Carlos Alberto Martorini Silveira <c7.silveira@gmail.com>
2020-08-20 13:34:38 -0300

    Create views and update class from Tenants

--- GIWEB/Controllers/TenantsController.cs
+++ GIWEB/Controllers/TenantsController.cs
@@ -1,16 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using GIWEB.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GIWEB.Models;
 
 namespace GIWEB.Controllers
 {
     public class TenantsController : Controller
     {
-        public IActionResult Index()
+        private readonly GIWEBContext _context;
+
+        public TenantsController(GIWEBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tenant.ToListAsync());
+        }
+
+        public ActionResult Details(int id)
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ GIWEB/Services/TenantService.cs
@@ -0,0 +1,70 @@
+﻿using GIWEB.Data;
+using GIWEB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GIWEB.Services.Exceptions;
+
+namespace GIWEB.Services
+{
+    public class TenantService
+    {
+        private readonly GIWEBContext _context;
+
+        public TenantService(GIWEBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Tenant>> FindAllAsync()
+        {
+            return await _context.Tenant.ToListAsync();
+        }
+
+        public async Task InsertAsync(Tenant obj)
+        {
+            _context.Add(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Tenant> FindByIdAsync(int id)
+        {
+            return await _context.Tenant.FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var obj = await _context.Tenant.FindAsync(id);
+                _context.Tenant.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
+        }
+
+        public async Task UpdateAsync(Tenant obj)
+        {
+            bool hasAny = await _context.Tenant.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny)
+            {
+                throw new NotFoundException("ID not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+    }
+}
+
